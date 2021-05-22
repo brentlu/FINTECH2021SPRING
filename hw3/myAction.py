2cb210f1bd68a39617898c4c8fee35357997f74b
Brent Lu <brent.lu@intel.com>
2021-05-22 15:51:01 +0800

    hw3: implement myAction01
    Implement myAction01 function which finds the optimal solution with
    2-dimension dp matrix.
    
    dp[i][j] means in day i our decision is holding stock j
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>

--- hw3/myAction.py
+++ hw3/myAction.py
@@ -78,7 +78,103 @@ def myActionSimple(priceMat, transFeeRate):
 
 # A DP-based approach to obtain the optimal return
 def myAction01(priceMat, transFeeRate):
+    # default
+    initCash = 1000
+    # user definition
+    dayNum, stockNum = priceMat.shape  # day size & stock count
+    dp = np.zeros((dayNum, stockNum+1), dtype=float)
+    bt = np.zeros((dayNum, stockNum+1), dtype=int)
     actionMat = []  # An k-by-4 action matrix which holds k transaction records.
+
+    # init the dp and bt matrix
+    day = 0
+    dayPrices = priceMat[day]  # Today price of each stock
+
+    for stock in range(0, stockNum):
+        dp[day][stock] = initCash*(1-transFeeRate)/dayPrices[stock]
+        bt[day][stock] = -1
+
+    dp[day][stockNum] = initCash
+    bt[day][stockNum] = -1
+
+    # compute the dp and bt matrix
+    for day in range(1, dayNum):
+        dayPrices = priceMat[day]  # Today price of each stock
+
+        for cashTo in range(0, stockNum+1):
+            for cashFrom in range(0, stockNum+1):
+                cashOutValue = dp[day-1][cashFrom]
+
+                if cashFrom != stockNum and cashFrom != cashTo:
+                    # sell stock
+                    cashOutValue = cashOutValue*dayPrices[cashFrom]*(1-transFeeRate)
+
+                buyIn = cashOutValue
+
+                if cashTo != stockNum and cashFrom != cashTo:
+                    # buy new stock
+                    buyIn = buyIn*(1-transFeeRate)/dayPrices[cashTo]
+
+                if buyIn > dp[day][cashTo]:
+                    dp[day][cashTo] = buyIn
+                    bt[day][cashTo] = cashFrom
+
+    #for day in range(0, dayNum):
+    #    print(dp[day])
+    #    print(bt[day])
+
+    # backtrace
+    holding = stockNum
+
+    cashDaysTotal = 0
+    cashDaysCount = 0
+    cashDaysContinue = 0
+
+    for day in range(dayNum-1, -1, -1):
+        dayPrices = priceMat[day]  # Today price of each stock
+
+        sellStock = -1
+        buyStock = -1
+        transValue = 0
+
+        if holding == stockNum:
+            cashDaysTotal += 1
+            cashDaysCount += 1
+        else:
+            if cashDaysCount > cashDaysContinue:
+                cashDaysContinue = cashDaysCount
+            cashDaysCount = 0
+
+        prevHolding = bt[day][holding]
+
+        if holding != stockNum and holding != prevHolding:
+            buyStock = holding
+            transValue = dp[day][buyStock]*dayPrices[buyStock]/(1-transFeeRate)
+
+        if prevHolding != stockNum and holding != prevHolding and prevHolding != -1:
+            sellStock = prevHolding
+            transValue = dp[day][sellStock]*dayPrices[sellStock]
+
+        if sellStock != -1 or buyStock != -1:
+            actionMat.append([day, sellStock, buyStock, transValue])
+
+        holding = prevHolding
+        if holding == -1:
+            cashDaysTotal += day
+            cashDaysCount += day
+            if cashDaysCount > cashDaysContinue:
+                cashDaysContinue = cashDaysCount
+            break
+
+    actionMat.reverse()
+
+    #for action in actionMat:
+    #    print(action)
+
+    #print('Submit: rr=%f%%' % ((actionMat[-1][3]-initCash)*100/initCash))
+    #print('Submit: Non continueous cash holding=%d' % (cashDaysTotal))
+    #print('Submit: Continueous cash holding=%d' %(cashDaysContinue))
+
     return actionMat
 
 # An approach that allow non-consecutive K days to hold all cash without any stocks
